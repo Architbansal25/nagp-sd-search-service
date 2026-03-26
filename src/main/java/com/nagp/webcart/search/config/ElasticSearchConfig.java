640a76f2bea8d2267374f78ea837618d9800aee8
architbansal <archit.bansal@@nagarro.com>
2026-03-27 00:16:17 +0530

    updated

--- src/main/java/com/nagp/webcart/search/config/ElasticSearchConfig.java
+++ src/main/java/com/nagp/webcart/search/config/ElasticSearchConfig.java
@@ -1,38 +1,28 @@
 package com.nagp.webcart.search.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.security.cert.X509Certificate;
-
 @Configuration
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
 
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("35.89.133.135:9200")// If authentication is required
+                .connectedTo(elasticsearchHost)
+                .withBasicAuth(username, password)
                 .build();
     }
 
-//    private SSLContext getSslContext() {
-//        try {
-//            TrustManager[] trustManagers = new TrustManager[]{
-//                    new X509TrustManager() {
-//                        public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-//                        public void checkServerTrusted(X509Certificate[] chain, String authType) {}
-//                        public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
-//                    }
-//            };
-//            SSLContext sslContext = SSLContext.getInstance("TLS");
-//            sslContext.init(null, trustManagers, new java.security.SecureRandom());
-//            return sslContext;
-//        } catch (Exception e) {
-//            throw new RuntimeException("Failed to create SSL context", e);
-//        }
-//    }
 }
